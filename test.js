f4a044bd27d89aaa24379525a6871639ba60eb3f
Ahmedafrican <120326240+Ahmedafrican@users.noreply.github>
2023-04-13 17:59:38 +0200

    Add test.js

--- test.js
+++ test.js
@@ -1,80 +1,99 @@
    // function of computer
         
    function getComputerChoice(){
-    return "ROCK";
-  }
+     const weapon = ["ROCK" , "SCISSORS" , "PAPER"];
+     const random = weapon[Math.floor(Math.random() * weapon.length)];
+     return random;
+    };
+    
+   
   
   // function of user
   
   function getUserChoice( ){
     let name = prompt("Write Your Shape");
     let up = name.toUpperCase();
+    return up ;
+    };
+    
     
   
-    return up ;
-    }
   // fucntion 0f decideResult
   function decideResult(userChoice , computerChoice) {
     if (userChoice === computerChoice) {
               return "draw";
-            } else if (userChoice === "ROCK" && computerChoice === "SCISSORS") {
+            }
+            else if (userChoice === "ROCK" && computerChoice === "SCISSORS") {
               return "player";
-            } else if (userChoice === "ROCK" && computerChoice === "PAPER") {
+            }
+            else if (userChoice === "ROCK" && computerChoice === "PAPER") {
               return "computer";
-            } else if (userChoice === "PAPER" && computerChoice === "ROCK") {
+            } 
+            else if (userChoice === "PAPER" && computerChoice === "ROCK") {
               return "player";
-            } else if (userChoice === "PAPER" && computerChoice === "SCISSORS") {
+            } 
+            else if (userChoice === "PAPER" && computerChoice === "SCISSORS") {
               return "computer";
-            } else if (userChoice === "SCISSORS" && computerChoice === "PAPER") {
+            } 
+            else if (userChoice === "SCISSORS" && computerChoice === "PAPER") {
               return "player";
-            } else if (userChoice=== "SCISSORS" && computerChoice === "ROCK") {
+            } 
+            else if (userChoice=== "SCISSORS" && computerChoice === "ROCK") {
               return "computer";
             }
-          }
+          };
+            
           // function of displayRoundResult 
           
           function displayRoundResult(roundResult){
             if (roundResult === "draw" ){
               console.log(" draw ");
-            }else if (roundResult === "player"){
+            }
+            else if (roundResult === "player"){
               console.log("The Player Won");
-            }else if (roundResult === "computer"){
+            }
+            else if (roundResult === "computer"){
               console.log("The Computer Won");
             }
-            
-          }
-        
-        
+          };
+           
+          
+          
           // for loop
-         let playerScore = 0 ;
-         let computerScore = 0 ;
+          let playerScore = 0 ;
+          let computerScore = 0 ;
           for (let i = 1; i < 6; i++){
             console.log("round" + " " + i);
-            let x = getComputerChoice();
-            let b = getUserChoice();
-            let c = decideResult(b, x);
-            displayRoundResult(c);
-            if(c === "computer"){
+            let computerSelect = getComputerChoice();
+            let userSelect = getUserChoice();
+            let result = decideResult(userSelect, computerSelect);
+            displayRoundResult(result);
+            if(result === "computer"){
              console.log(`${'computer score ='} ${computerScore+=1}`);
              console.log(`${"player score ="} ${playerScore}`);
-            }else if (c === "player"){
+            }
+            else if (result === "player"){
               console.log(`${"player score ="} ${playerScore+=1}`);
               console.log(`${`computer score =`} ${computerScore}`);
-            }else{
+            }
+            else{
               console.log(`${'computer score ='} ${computerScore}`);
               console.log(`${"player score ="} ${playerScore}`);
-            }if (computerScore === 3){
-              break;
-            }else if(playerScore === 3){
+            }
+            if (computerScore === 3){
               break;
             }
-         }
-         
-         if (computerScore > playerScore){
-              console.log("The Computer is the winner");
-            }else if (playerScore > computerScore){
-              console.log("The Player is the winner");
-            }else{
-              console.log("The Player draw with the Computer");
+            else if(playerScore === 3){
+              break;
             }
-  
+          };
+          if (computerScore > playerScore){
+               console.log("The Computer is the winner");
+             }
+             else if (playerScore > computerScore){
+               console.log("The Player is the winner");
+             }
+             else{
+               console.log("The Player draw with the Computer");
+             };
+            
